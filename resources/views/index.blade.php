81ec7a0556eb8f15d29562653279e39f32bd39bc
GirtsE <geihvalds@gmail.com>
2022-12-14 12:18:13 +0200

    template Navbar and style

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('templates.main')
+
+@section('MainContent')
+    <div id="Indexcontainer">
+        <h2>Digital<h3>Marketing</h3></h2>
+        
+        <p> 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Non deleniti nemo perspiciatis delectus nulla.
+            Repellendus ducimus quidem accusamus obcaecati dolores autem. 
+            Magni distinctio officiis veniam nesciunt porro eaque expedita. Architecto.
+        </p>
+    </div>
+    <img src="storage/Images/illustration.jpg" alt="IMG">
+
+@stop
---
+++ resources/views/templates/main.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/Global.css">
+    <link rel="stylesheet" href="css/MainTemplateStyle.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav id="mainNavBar">
+        <div id="logo">
+            <h1>LOGO</h1>
+        </div>
+        <div id="mainNavBarLinks">
+            <a href="{{ url('/') }}" class="link">Home</a>
+            <a href="{{ url('/') }}" class="link">Seevices</a>
+            <a href="{{ url('/') }}" class="link">About</a>
+            <a href="{{ url('/') }}" class="link">Contact</a>
+            <a href="{{ url('/') }}" class="link">FAQ</a>
+            <a href="{{ url('SignUp') }}" class="link SignupLink">SIGN UP</a>
+        </div>
+    </nav>
+
+    <main>
+        @yield('MainContent')
+    </main>
+
+    
+</body>
+</html>
